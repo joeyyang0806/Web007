63edcfe0cc8d137a7facc3642774d38fa2f280c1
joeyyang0806 <rossyy@126.com>
2020-06-08 23:29:39 +0800

    commit

--- detector/src/main/java/com/detector/util/HttpClientUtil.java
+++ detector/src/main/java/com/detector/util/HttpClientUtil.java
@@ -1,11 +1,7 @@
 package com.detector.util;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -15,74 +11,83 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpClientUtil {
-	public static boolean getBase1ReponseString() throws ClientProtocolException, IOException {
-		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		params.add(new BasicNameValuePair("loginname", "admin01"));
-		params.add(new BasicNameValuePair("pwd", "Dtt!234567890"));
-		HttpEntity httpEntity = new UrlEncodedFormEntity(params, "UTF-8");
-		
-		RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(5000).setSocketTimeout(5000).setConnectionRequestTimeout(5000).build();
-		
-		HttpClient httpClient = new DefaultHttpClient();
-		HttpPost post = new HttpPost("http://10.173.20.43:10010/BASE/randd/ibizutil/login.do");
-		post.setEntity(httpEntity);
-		post.setConfig(requestConfig);
-		
-		HttpResponse response = httpClient.execute(post);
-		if(response.getStatusLine().getStatusCode() == 200) {
-			HttpEntity resEntity = response.getEntity();
-			String message = EntityUtils.toString(resEntity, "utf-8"); 
-			JSONObject jsonObj =  JSON.parseObject(message);
-			if(jsonObj.get("isSa").toString().equals("1")) {
-				return true;
-			}
-			return false;
-		}
-		
-		else {
-			System.out.println("«Î«Û ß∞‹");
-		}
-		
-		
-		return false;
-	}
-	
-	
-	public static boolean getBase2ResponseString() throws Exception, IOException {
-		List<NameValuePair> list = new ArrayList<NameValuePair>();
-		list.add(new BasicNameValuePair("loginname", "admin01"));
-		list.add(new BasicNameValuePair("pwd", "Dtt!234567890"));
-		HttpEntity entity = new UrlEncodedFormEntity(list, "UTF-8");
-		
-		RequestConfig config = RequestConfig.custom().setConnectTimeout(5000).setConnectionRequestTimeout(5000).setSocketTimeout(5000).build();
-		HttpPost post = new HttpPost();
-		post.setURI(URI.create("http://10.173.20.44:10010/BASE/randd/ibizutil/login.do"));
-		post.setEntity(entity);
-		post.setConfig(config);
-		
-		HttpClient client = new DefaultHttpClient();
-		HttpResponse response = client.execute(post);
-		
-		if(response.getStatusLine().getStatusCode()==200) {
-			HttpEntity res= response.getEntity();
-			String resStr = EntityUtils.toString(res, "UTF-8");
-			JSONObject json = JSON.parseObject(resStr);
-			if(json.getString("isSa").equals("1")) {
-				return true;
-			}
-		}
-		return false;
-	}
-	public static void main(String args[]) throws Exception {
-		boolean meetError = getBase1ReponseString();
-		meetError = getBase2ResponseString();
-		add some error;
-	}
+    public static boolean getBase1ReponseString() throws ClientProtocolException, IOException {
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("loginname", "admin01"));
+        params.add(new BasicNameValuePair("pwd", "Dtt!234567890"));
+        HttpEntity httpEntity = new UrlEncodedFormEntity(params, "UTF-8");
+
+        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(5000).setSocketTimeout(5000).setConnectionRequestTimeout(5000).build();
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://10.173.20.43:10010/BASE/randd/ibizutil/login.do");
+        post.setEntity(httpEntity);
+        post.setConfig(requestConfig);
+
+        HttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == 200) {
+            HttpEntity resEntity = response.getEntity();
+            String message = EntityUtils.toString(resEntity, "utf-8");
+            JSONObject jsonObj = JSON.parseObject(message);
+            if (jsonObj.get("isSa").toString().equals("1")) {
+                return true;
+            }
+            return false;
+        } else {
+            System.out.println("ÂèëÁîüÈîôËØØ„ÄÇ");
+        }
+
+
+        return false;
+    }
+
+
+    public static boolean getBase2ResponseString() throws Exception, IOException {
+        List<NameValuePair> list = new ArrayList<NameValuePair>();
+        list.add(new BasicNameValuePair("loginname", "admin01"));
+        list.add(new BasicNameValuePair("pwd", "Dtt!234567890"));
+        HttpEntity entity = new UrlEncodedFormEntity(list, "UTF-8");
+
+        RequestConfig config = RequestConfig.custom().setConnectTimeout(5000).setConnectionRequestTimeout(5000).setSocketTimeout(5000).build();
+        HttpPost post = new HttpPost();
+        post.setURI(URI.create("http://10.173.20.44:10010/BASE/randd/ibizutil/login.do"));
+        post.setEntity(entity);
+        post.setConfig(config);
+
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(post);
+
+        if (response.getStatusLine().getStatusCode() == 200) {
+            HttpEntity res = response.getEntity();
+            String resStr = EntityUtils.toString(res, "UTF-8");
+            JSONObject json = JSON.parseObject(resStr);
+            if (json.getString("isSa").equals("1")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]) throws Exception {
+        boolean meetError = getBase1ReponseString();
+        meetError = getBase2ResponseString();
+    }
+
+    private static PoolingHttpClientConnectionManager connectionManager = null;
+    private static HttpClientBuilder clientBuilder = null;
+    private static RequestConfig requestConfig = null;
+    private static int maxConnection = 10;
+    private static int defaultMaxConnection = 5;
+
 }
--- detector/src/main/java/com/detector/util/JsonUtil.java
+++ detector/src/main/java/com/detector/util/JsonUtil.java
@@ -1,11 +1,13 @@
 package com.detector.util;
 
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 
 public class JsonUtil {
 	public static String getJsonReponseString() throws MalformedURLException {
 		URL url = new URL("");
+		URI uri = URI.create("");
 		return null;
 	}
 }
